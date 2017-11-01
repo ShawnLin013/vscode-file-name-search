3d4169590065b2277906248608bf779687e315d7
Shawn <hn62257@gmail.com>
2017-11-02 03:44:41 +0800

    Update command name

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ const vscode = require('vscode');
 const ncp = require("copy-paste");
 
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function (fileUri) {
+    let disposable = vscode.commands.registerCommand('extension.searchFileName', function (fileUri) {
         if (fileUri != undefined) {
             var path = fileUri.fsPath.split("\"")[0];
             var name = path.split("\\")[path.split("\\").length - 1];
