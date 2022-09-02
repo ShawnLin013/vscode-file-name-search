694f3d866cd524d5118eeb4db6f5a8099d426763
Per Oestergaard <per.oestergaard@lego.com>
2022-09-02 11:43:49 +0200

    Adding extension.searchFileNameWithoutExtension

--- extension.js
+++ extension.js
@@ -2,10 +2,14 @@ const vscode = require('vscode');
 const ncp = require("copy-paste");
 const path = require("path");
 
-function addCommand(context, command, matchCase, matchWholeWord) {
+function addCommand(context, command, matchCase, matchWholeWord, withoutExtension) {
+    console.log('test');
     let searchFileName = vscode.commands.registerCommand(command, function (fileUri) {
         if (fileUri != undefined) {
             var name = path.basename(fileUri.fsPath);
+            if (withoutExtension) {
+                name=path.parse(name).name
+            }
             ncp.copy(name, function() {
                 vscode.commands.executeCommand('workbench.action.findInFiles', {
                     query: name,
@@ -20,10 +24,11 @@ function addCommand(context, command, matchCase, matchWholeWord) {
 }
 
 function activate(context) {
-    addCommand(context, 'extension.searchFileName', false, false);
-    addCommand(context, 'extension.searchFileName.matchCase', true, false);
-    addCommand(context, 'extension.searchFileName.matchWholeWord', false, true);
-    addCommand(context, 'extension.searchFileName.matchCaseAndWholeWord', true, true);
+    addCommand(context, 'extension.searchFileName', false, false,false);
+    addCommand(context, 'extension.searchFileNameWithoutExtension', false, false, true);
+    addCommand(context, 'extension.searchFileName.matchCase', true, false, false);
+    addCommand(context, 'extension.searchFileName.matchWholeWord', false, true, false);
+    addCommand(context, 'extension.searchFileName.matchCaseAndWholeWord', true, true, false);
 }
 exports.activate = activate;
 
